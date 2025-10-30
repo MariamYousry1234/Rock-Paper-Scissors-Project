f62bdb5e4567873583c932329476dc964cc15b08
MariamYousry1234 <mariamyousry030@gmail.com>
2025-10-30 11:14:13 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace Rock_Paper_Scissors_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblPlayerScore = new System.Windows.Forms.Label();
+            this.lblComputerScore = new System.Windows.Forms.Label();
+            this.lblPlayer = new System.Windows.Forms.Label();
+            this.lblComputer = new System.Windows.Forms.Label();
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnScissors = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblGameCount = new System.Windows.Forms.Label();
+            this.lblPlayerChoice = new System.Windows.Forms.Label();
+            this.lblComputerChoice = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(276, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(368, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rock Paper Scissors";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(359, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "With Computer";
+            // 
+            // lblPlayerScore
+            // 
+            this.lblPlayerScore.AutoSize = true;
+            this.lblPlayerScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayerScore.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerScore.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblPlayerScore.Location = new System.Drawing.Point(292, 171);
+            this.lblPlayerScore.Name = "lblPlayerScore";
+            this.lblPlayerScore.Size = new System.Drawing.Size(74, 77);
+            this.lblPlayerScore.TabIndex = 2;
+            this.lblPlayerScore.Text = "0";
+            // 
+            // lblComputerScore
+            // 
+            this.lblComputerScore.AutoSize = true;
+            this.lblComputerScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblComputerScore.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComputerScore.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblComputerScore.Location = new System.Drawing.Point(506, 171);
+            this.lblComputerScore.Name = "lblComputerScore";
+            this.lblComputerScore.Size = new System.Drawing.Size(74, 77);
+            this.lblComputerScore.TabIndex = 3;
+            this.lblComputerScore.Text = "0";
+            // 
+            // lblPlayer
+            // 
+            this.lblPlayer.AutoSize = true;
+            this.lblPlayer.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayer.Font = new System.Drawing.Font("Tahoma", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer.ForeColor = System.Drawing.Color.Ivory;
+            this.lblPlayer.Location = new System.Drawing.Point(285, 280);
+            this.lblPlayer.Name = "lblPlayer";
+            this.lblPlayer.Size = new System.Drawing.Size(92, 45);
+            this.lblPlayer.TabIndex = 4;
+            this.lblPlayer.Tag = "0";
+            this.lblPlayer.Text = "You";
+            // 
+            // lblComputer
+            // 
+            this.lblComputer.AutoSize = true;
+            this.lblComputer.BackColor = System.Drawing.Color.Transparent;
+            this.lblComputer.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComputer.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblComputer.Location = new System.Drawing.Point(469, 285);
+            this.lblComputer.Name = "lblComputer";
+            this.lblComputer.Size = new System.Drawing.Size(175, 39);
+            this.lblComputer.TabIndex = 5;
+            this.lblComputer.Tag = "0";
+            this.lblComputer.Text = "Computer";
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.AutoSize = true;
+            this.lblWinner.BackColor = System.Drawing.Color.Transparent;
+            this.lblWinner.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinner.ForeColor = System.Drawing.Color.Fuchsia;
+            this.lblWinner.Location = new System.Drawing.Point(355, 440);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(198, 39);
+            this.lblWinner.TabIndex = 6;
+            this.lblWinner.Text = "Start Game";
+            // 
+            // btnRock
+            // 
+            this.btnRock.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnRock.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRock.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRock.Location = new System.Drawing.Point(191, 526);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(119, 29);
+            this.btnRock.TabIndex = 7;
+            this.btnRock.Text = "Rock";
+            this.btnRock.UseVisualStyleBackColor = false;
+            this.btnRock.Click += new System.EventHandler(this.PlayerChoice_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnPaper.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPaper.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPaper.Location = new System.Drawing.Point(374, 526);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(119, 29);
+            this.btnPaper.TabIndex = 8;
+            this.btnPaper.Text = "Paper";
+            this.btnPaper.UseVisualStyleBackColor = false;
+            this.btnPaper.Click += new System.EventHandler(this.PlayerChoice_Click);
+            // 
+            // btnScissors
+            // 
+            this.btnScissors.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnScissors.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnScissors.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScissors.Location = new System.Drawing.Point(561, 526);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(119, 29);
+            this.btnScissors.TabIndex = 9;
+            this.btnScissors.Text = "Scissors";
+            this.btnScissors.UseVisualStyleBackColor = false;
+            this.btnScissors.Click += new System.EventHandler(this.PlayerChoice_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.White;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(374, 588);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(115, 33);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(278, 672);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(176, 29);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Game Count: ";
+            // 
+            // lblGameCount
+            // 
+            this.lblGameCount.AutoSize = true;
+            this.lblGameCount.BackColor = System.Drawing.Color.Transparent;
+            this.lblGameCount.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameCount.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblGameCount.Location = new System.Drawing.Point(460, 668);
+            this.lblGameCount.Name = "lblGameCount";
+            this.lblGameCount.Size = new System.Drawing.Size(37, 39);
+            this.lblGameCount.TabIndex = 12;
+            this.lblGameCount.Tag = "0";
+            this.lblGameCount.Text = "0";
+            // 
+            // lblPlayerChoice
+            // 
+            this.lblPlayerChoice.BackColor = System.Drawing.Color.Transparent;
+            this.lblPlayerChoice.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerChoice.ForeColor = System.Drawing.Color.Lime;
+            this.lblPlayerChoice.Location = new System.Drawing.Point(288, 360);
+            this.lblPlayerChoice.Name = "lblPlayerChoice";
+            this.lblPlayerChoice.Size = new System.Drawing.Size(106, 45);
+            this.lblPlayerChoice.TabIndex = 13;
+            this.lblPlayerChoice.Tag = "0";
+            // 
+            // lblComputerChoice
+            // 
+            this.lblComputerChoice.BackColor = System.Drawing.Color.Transparent;
+            this.lblComputerChoice.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComputerChoice.ForeColor = System.Drawing.Color.Lime;
+            this.lblComputerChoice.Location = new System.Drawing.Point(515, 362);
+            this.lblComputerChoice.Name = "lblComputerChoice";
+            this.lblComputerChoice.Size = new System.Drawing.Size(92, 45);
+            this.lblComputerChoice.TabIndex = 14;
+            this.lblComputerChoice.Tag = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::Rock_Paper_Scissors_Project.Properties.Resources.Photo;
+            this.ClientSize = new System.Drawing.Size(898, 724);
+            this.Controls.Add(this.lblComputerChoice);
+            this.Controls.Add(this.lblPlayerChoice);
+            this.Controls.Add(this.lblGameCount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnScissors);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnRock);
+            this.Controls.Add(this.lblWinner);
+            this.Controls.Add(this.lblComputer);
+            this.Controls.Add(this.lblPlayer);
+            this.Controls.Add(this.lblComputerScore);
+            this.Controls.Add(this.lblPlayerScore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Rock Paper Scissors Game";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblPlayerScore;
+        private System.Windows.Forms.Label lblComputerScore;
+        private System.Windows.Forms.Label lblPlayer;
+        private System.Windows.Forms.Label lblComputer;
+        private System.Windows.Forms.Label lblWinner;
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnScissors;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblGameCount;
+        private System.Windows.Forms.Label lblPlayerChoice;
+        private System.Windows.Forms.Label lblComputerChoice;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rock_Paper_Scissors_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        enum enChoice
+        {
+            Rock,
+            Paper,
+            Scissors
+        }
+
+        enum enWinner
+        {
+            Player,
+            Computer,
+            Draw
+        }
+        struct stGameInfo
+        {
+            public enChoice PlayerChoice;
+            public enChoice ComputerChoice;
+            public enWinner Winner;
+           
+        }
+
+        void ResetGame()
+        {
+         
+            lblPlayer.Tag = 0;
+            lblComputer.Tag = 0;
+            lblPlayerScore.Text = "0";
+            lblComputerScore.Text = "0";
+
+            lblComputerChoice.Text = "";
+            lblPlayerChoice.Text = "";
+
+            lblWinner.Text = "Start Game";
+
+            lblGameCount.Tag = 0;
+            lblGameCount.Text = "0";
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Color White = Color.FromArgb(250, 250, 250, 250);
+            Pen pen = new Pen(White);
+
+            pen.Width = 3;
+
+            pen.StartCap = System.Drawing.Drawing2D.LineCap.Flat;
+            pen.EndCap = System.Drawing.Drawing2D.LineCap.Flat;
+
+            e.Graphics.DrawLine(pen, 430, 140, 430, 400);
+        }
+
+        enChoice GetComputerChoice()
+        {
+            Random random = new Random();
+            return (enChoice)random.Next(0, 3);
+        }
+
+        enWinner WhoIsWinner(enChoice ComputerChoice,enChoice PlayerChoice)
+        {
+            if (ComputerChoice == PlayerChoice)
+                return enWinner.Draw;
+
+            if ((ComputerChoice == enChoice.Rock && PlayerChoice == enChoice.Scissors) ||
+                (ComputerChoice == enChoice.Scissors && PlayerChoice == enChoice.Paper) ||
+                (ComputerChoice == enChoice.Paper && PlayerChoice == enChoice.Rock))
+                return enWinner.Computer;
+
+            return enWinner.Player;
+
+        }
+
+        enChoice ReadPlayerChoice(Button PlayerChoice)
+        {
+            if (PlayerChoice == btnRock)
+            {
+               return enChoice.Rock;
+            }
+
+            if (PlayerChoice == btnPaper)
+            {
+                return enChoice.Paper;
+            }
+
+            return enChoice.Scissors;
+         
+        }
+
+        void ChangeGameScore(enWinner Winner)
+        {
+            if (Winner == enWinner.Player)
+                lblPlayer.Tag = Convert.ToInt32(lblPlayer.Tag) + 1;
+
+            if (Winner == enWinner.Computer)
+                lblComputer.Tag = Convert.ToInt32(lblComputer.Tag) + 1;
+
+
+        }
+
+        string GetStringChoice(enChoice Choice)
+        {
+            switch (Choice)
+            {
+                case enChoice.Rock:
+                    return "Rock";
+
+                case enChoice.Paper:
+                    return "Paper";
+
+                default:
+                    return "Scissors";
+            }
+        }
+
+        void PrintGameInfo(stGameInfo GameInfo)
+        {
+            lblGameCount.Text = lblGameCount.Tag.ToString();
+            lblPlayerChoice.Text = GetStringChoice(GameInfo.PlayerChoice);
+            lblComputerChoice.Text = GetStringChoice(GameInfo.ComputerChoice);
+            lblPlayerScore.Text = lblPlayer.Tag.ToString();
+            lblComputerScore.Text = lblComputer.Tag.ToString();
+
+            if (GameInfo.Winner == enWinner.Player)
+            {
+                lblWinner.Text = "You Win";
+                return;
+            }
+            if (GameInfo.Winner == enWinner.Computer)
+            {
+                lblWinner.Text = "You Lose";
+                return;
+            }
+
+            lblWinner.Text = "   Draw";
+
+        }
+
+        stGameInfo GameInfo(Button PlayerChoice)
+        {
+            stGameInfo GameInfo = new stGameInfo();
+
+            lblGameCount.Tag = Convert.ToInt32(lblGameCount.Tag) + 1;
+            GameInfo.PlayerChoice = ReadPlayerChoice(PlayerChoice);
+            GameInfo.ComputerChoice = GetComputerChoice();
+            GameInfo.Winner = WhoIsWinner(GameInfo.ComputerChoice, GameInfo.PlayerChoice);
+            ChangeGameScore(GameInfo.Winner);
+
+            return GameInfo;
+        }
+
+        void StartGame(Button PlayerChoice)
+        {
+            PrintGameInfo(GameInfo(PlayerChoice));
+        }
+
+        private void PlayerChoice_Click(object sender, EventArgs e)
+        {
+            StartGame((Button)sender);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ResetGame();
+        }
+
+      
+    }
+}
